02224b9cb89bef4b15d3791a72bca4cf4af0e877
Nishi Kaza <nishikaza@berkeley.edu>
2020-05-11 16:17:19 -0700

    syncing git repo

---
+++ run.py
@@ -0,0 +1,14 @@
+from parse import *
+import networkx as nx
+import os
+from utils import is_valid_network
+from solver import solve
+
+if __name__ == "__main__":
+    output_dir = "outputs"
+    input_dir = "inputs"
+    for input_path in os.listdir(input_dir):
+        graph_name = input_path.split(".")[0]
+        G = read_input_file(f"{input_dir}/{input_path}")
+        T = solve(G)
+        write_output_file(T, f"{output_dir}/{graph_name}.out")
